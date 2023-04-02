b4c1b425db0adac8fad52dc857df59b9e5d67f9d
DmytroSoldatov <cyberdexx@gmail.com>
2023-04-02 21:18:49 +0300

    revoltfx wip

--- src/games/FireGame.ts
+++ src/games/FireGame.ts
@@ -3,6 +3,7 @@ import { IGame } from "./IGame";
 import { GameBase } from "./GameBase";
 import config from "../config/fireGameConfig";
 import { Assets } from "@pixi/assets";
+import { Loader } from '@pixi/loaders';
 
 export class FireGame extends GameBase implements IGame {
     paused = false;
@@ -16,6 +17,42 @@ export class FireGame extends GameBase implements IGame {
     async init() {
         await Assets.loadBundle('fire');
 
+        const loader = Loader.shared;
+        
+        // loader.add('fx_settings', 'assets/default-bundle.json');
+        // loader.add('fx_spritesheet', 'assets/revoltfx-spritesheet.json');
+        // loader.add('example_spritesheet', 'assets/rfx-examples.json');
+        
+        // loader.load(function (loader, resources) {
+
+        //         console.log({
+        //             loader,
+        //             resources
+        //         });
+                
+        //         //Init the bundle
+        //         // fx.initBundle(resources.fx_settings.data);
+
+        //         // var content = new PIXI.Container();
+        //         // content.x = width * 0.5;
+        //         // content.y = height * 0.5;
+        //         // app.stage.addChild(content);
+
+        //         // var logo = PIXI.Sprite.from('logo');
+        //         // logo.anchor.set(0.5);
+        //         // logo.alpha = 0.6;
+        //         // content.addChild(logo);
+
+        //         // var emitter = fx.getParticleEmitter('plasma-corona');
+        //         // emitter.init(content, true, 1.9);
+
+        //         // app.ticker.add(function (delta) {
+        //         //     //Update the RevoltFX instance
+        //         //     fx.update(delta);
+        //         // });
+
+        //     });
+        
         this.start();
     }
 
