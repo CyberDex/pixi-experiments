cd486e7d33134d29498842b4cbfd7206a986e699
DmytroSoldatov <cyberdexx@gmail.com>
2023-04-03 00:11:29 +0300

    improve fire

--- src/config/fireGameConfig.ts
+++ src/config/fireGameConfig.ts
@@ -1,4 +1,276 @@
-export default {
-    width: 600,
-    height: 600,
-}
+export const fireWidthSmoke = (x: number, y: number, w: number, h: number) => ({
+    "lifetime": {
+        "min": 0.5,
+        "max": 0.7
+    },
+    "frequency": 0.00008,
+    "emitterLifetime": 0,
+    "maxParticles": 10000,
+    "addAtBack": false,
+    "pos": {
+        "x": 0,
+        "y": 0
+    },
+    "behaviors": [
+        {
+            "type": "alpha",
+            "config": {
+                "alpha": {
+                    "list": [
+                        {
+                            "value": 0.62,
+                            "time": 0
+                        },
+                        {
+                            "value": 0,
+                            "time": 0.6
+                        },
+                        {
+                            "value": 0,
+                            "time": 0.7
+                        },
+                        {
+                            "value": 0.8,
+                            "time": 0.71
+                        },
+                        {
+                            "value": 0,
+                            "time": 1
+                        }
+                    ],
+                    "isStepped": false
+                }
+            }
+        },
+        {
+            "type": "moveSpeed",
+            "config": {
+                "speed": {
+                    "list": [
+                        {
+                            "value": 500,
+                            "time": 0
+                        },
+                        {
+                            "value": 450,
+                            "time": 0.7
+                        },
+                        {
+                            "value": 450,
+                            "time": 1
+                        }
+                    ],
+                    "isStepped": true
+                },
+                "minMult": 1
+            }
+        },
+        {
+            "type": "scale",
+            "config": {
+                "scale": {
+                    "list": [
+                        {
+                            "value": 0.25,
+                            "time": 0
+                        },
+                        {
+                            "value": 0.75,
+                            "time": 1
+                        }
+                    ],
+                    "isStepped": false
+                },
+                "minMult": 1
+            }
+        },
+        {
+            "type": "color",
+            "config": {
+                "color": {
+                    "list": [
+                        {
+                            "value": "fff191",
+                            "time": 0
+                        },
+                        {
+                            "value": "ff622c",
+                            "time": 0.6
+                        },
+                        {
+                            "value": "333232",
+                            "time": 0.7
+                        },
+                        {
+                            "value": "333333",
+                            "time": 1
+                        }
+                    ],
+                    "isStepped": false
+                }
+            }
+        },
+        {
+            "type": "rotation",
+            "config": {
+                "accel": 0,
+                "minSpeed": 50,
+                "maxSpeed": 50,
+                "minStart": 265,
+                "maxStart": 275
+            }
+        },
+        {
+            "type": "textureRandom",
+            "config": {
+                "textures": fireTextures
+            }
+        },
+        {
+            "type": "spawnShape",
+            "config": {
+                type: 'rect',
+                data: {
+                    x,
+                    y,
+                    w,
+                    h,
+                }
+            }
+        }
+    ]
+});
+
+export type Quality = 'low' | 'medium' | 'high';
+
+export const fireConfig = (
+    width: number,
+    quality: Quality
+) => {
+    let frequency = 0.00008;
+    let maxParticles = 10000;
+
+    switch (quality) { 
+        case 'low':
+            frequency = 0.0008
+            maxParticles = 1000;
+            break;
+        case 'medium':
+            frequency = 0.00008;
+            maxParticles = 1000;
+            break;
+        case 'high':
+            frequency = 0.00008;
+            maxParticles = 10000;
+            break;
+    }
+
+    return {
+        "lifetime": {
+            "min": 0.5,
+            "max": 0.7
+        },
+        "frequency": frequency,
+        "emitterLifetime": 0,
+        "maxParticles": maxParticles,
+        "addAtBack": false,
+        "pos": {
+            "x": 0,
+            "y": 0
+        },
+        "behaviors": [
+            {
+                "type": "alpha",
+                "config": {
+                    "alpha": {
+                        "list": [
+                            {
+                                "time": 0,
+                                "value": 0.62
+                            },
+                            {
+                                "time": 1,
+                                "value": 0
+                            }
+                        ]
+                    }
+                }
+            },
+            {
+                "type": "moveSpeedStatic",
+                "config": {
+                    "min": 500,
+                    "max": 500
+                }
+            },
+            {
+                "type": "scale",
+                "config": {
+                    "scale": {
+                        "list": [
+                            {
+                                "time": 0,
+                                "value": 0.25
+                            },
+                            {
+                                "time": 1,
+                                "value": 0.75
+                            }
+                        ]
+                    },
+                    "minMult": 1
+                }
+            },
+            {
+                "type": "color",
+                "config": {
+                    "color": {
+                        "list": [
+                            {
+                                "time": 0,
+                                "value": "fff191"
+                            },
+                            {
+                                "time": 1,
+                                "value": "ff622c"
+                            }
+                        ]
+                    }
+                }
+            },
+            {
+                "type": "rotation",
+                "config": {
+                    "accel": 0,
+                    "minSpeed": 50,
+                    "maxSpeed": 50,
+                    "minStart": 265,
+                    "maxStart": 275
+                }
+            },
+            {
+                "type": "textureRandom",
+                "config": {
+                    "textures": fireTextures
+                }
+            },
+            {
+                "type": "spawnShape",
+                "config": {
+                    type: 'rect',
+                    data: {
+                        x: 0,
+                        y: 0,
+                        w: width,
+                        h: 100,
+                    }
+                }
+            }
+        ]
+    }
+};
+
+export const fireTextures = [
+    "assets/particle.png",
+    "assets/Fire.png"
+]
--- src/games/FireGame.ts
+++ src/games/FireGame.ts
@@ -1,14 +1,26 @@
 import { AppScreen } from "../components/basic/AppScreen";
 import { IGame } from "./IGame";
 import { GameBase } from "./GameBase";
-import config from "../config/fireGameConfig";
+import { Quality, fireConfig } from "../config/fireGameConfig";
 import { Emitter } from "@pixi/particle-emitter";
 import { Assets } from "@pixi/assets";
-import { fireParticleConfig } from "../config/fireParticleConfig";
+import { TilingSprite } from "@pixi/sprite-tiling";
+import { Texture } from "@pixi/core";
+import { app } from "../main"
 
 export class FireGame extends GameBase implements IGame {
     private emitter!: Emitter;
     private elapsed: number = 0;
+    private tint!: TilingSprite;
+    private quality: Quality = 'low';
+    private safeQuality!: Quality;
+    private fps: {
+        low: number;
+        high: number;
+    } = {
+        low: 0,
+        high: 0,
+    };
 
     paused = false;
     activated = false;
@@ -21,20 +33,101 @@ export class FireGame extends GameBase implements IGame {
     async init() {
         Assets.loadBundle('fire');
 
-        this.emitter = new Emitter(
-            this,
-            fireParticleConfig(0, 0, window.innerWidth, 100)
-        );
-
-        this.elapsed = Date.now();
-        
-        this.emitter.emit = true;
+        this.addViews();
 
         this.start();
     }
 
-    private bern() { 
+    private addViews() { 
+        this.tint = new TilingSprite(
+            Texture.from('pixi-logo'),
+            200,
+            200);
+        
+        this.addChild(this.tint);
+    }
 
+    private bern() { 
+        this.createEmitter(this.quality);
+        
+        this.elapsed = Date.now();
+        
+        this.emitter.emit = true;
+    }
+
+    private createEmitter(quality: Quality) {
+        this.quality = quality;
+
+        // TODO: update emitter settings without destroy
+        if (this.emitter) {
+            this.emitter.destroy();
+        }
+
+        this.emitter = new Emitter(
+            this,
+            fireConfig(window.innerWidth, quality)
+        );
+    }
+
+    private qualityDown() {
+        if (this.quality === 'low') { 
+            return;
+        } else if (this.quality === 'medium') {
+            this.quality = 'low';
+        } else if (this.quality === 'high') {
+            this.quality = 'medium';
+        }
+
+        console.log('quality', this.quality);
+
+        this.safeQuality = this.quality;
+
+        this.fps.low = 0;
+
+        this.createEmitter(this.quality);
+    }
+
+    private qualityUp() {
+        if (this.quality === 'high') { 
+            return;
+        } else if (this.quality === 'low') {
+            this.quality = 'medium';
+        } else if (this.quality === 'medium') {
+            this.quality = 'high';
+        }
+
+        console.log('quality', this.quality);
+
+        this.fps.high = 0;
+        
+        this.createEmitter(this.quality);
+    }
+
+    // TODO: improve quality adjust, use more frequency & maxParticles states
+    private adjustQuality() {
+        // console.log(this.fps, this.quality);
+        
+        if (this.quality !== 'low' && app.ticker.FPS < 60) { 
+            this.fps.low++;
+
+            if (this.fps.low > 10) {
+                this.qualityDown();
+            }
+        }
+
+        if (this.safeQuality) { 
+            return;
+        }
+
+        if (this.quality !== 'high') {
+            if (app.ticker.FPS && app.ticker.FPS > 100) {
+                this.fps.high++;
+
+                if (this.fps.high > 300) {
+                    this.qualityUp();
+                }
+            }
+        }
     }
 
     start() {
@@ -53,19 +146,16 @@ export class FireGame extends GameBase implements IGame {
     update() { 
         const now = Date.now();
         this.emitter.update((now - this.elapsed) * 0.001);
-        
+
+        this.adjustQuality();
+
         this.elapsed = now;
     }
     
     resize(width: number, height: number): void {
         this.x = 0;
         this.y = height;
-        
-        this.emitter.destroy();
 
-        this.emitter = new Emitter(
-            this,
-            fireParticleConfig(0, 0, window.innerWidth, 100)
-        );
+        this.createEmitter(this.quality);
     }
 }
